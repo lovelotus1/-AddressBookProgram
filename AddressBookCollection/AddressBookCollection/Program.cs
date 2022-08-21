55b6469744156086de42a5cb44907f1a7d654d15
“Love <opera.prem@gmail.com>
2022-08-21 11:28:51 +0430

    Added: Welcome Message

---
+++ AddressBookCollection/AddressBookCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Collection");
+        }
+    }
+}
